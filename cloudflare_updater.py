c94084894a30b373a681c836494ddb16e5f71099
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2025-05-27 21:42:16 +0200

    update env reading

--- cloudflare_updater.py
+++ cloudflare_updater.py
@@ -20,8 +20,10 @@ CF_EMAIL: str = os.getenv("CF_EMAIL")
 CF_RECORD_NAME: str = os.getenv("CF_RECORD_NAME")
 CF_RECORD_TYPE: str = os.getenv("CF_RECORD_TYPE", "A")
 CF_TTL: int = os.getenv("CF_TTL", 360)
-CF_PROXIED: bool = os.getenv("CF_PROXIED", False)
+CF_PROXIED: bool = os.getenv("CF_PROXIED", "false").lower() in ('true', '1', 'yes')
 
+
+print(CF_ZONE_ID)
 
 @dataclass
 class CloudflareDNSRecord:
